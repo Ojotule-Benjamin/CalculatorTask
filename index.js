6fc1ef260126890f1bf4d7a9c37021d52dd48e06
Ojotule <benjamin.ojotule.ajodo@gmail.com>
2022-05-23 18:45:22 +0100

    A simple calculator that performs basic arithmetic functions

---
+++ index.js
@@ -0,0 +1,28 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+//mapping to get value from each action
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '←':
+                if(display.innerText){
+                    display.innerText = display.innerText.slice(0, -1) //slice is a string method used to remove last element
+                }
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch{
+                    display.innerText = 'Error!'
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
